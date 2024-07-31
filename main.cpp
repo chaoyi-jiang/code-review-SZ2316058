0efaa781bf596e1e742401fcf8c4b1cbabda40de
unknown <1677325765@qq.com>
2024-07-31 10:24:47 +0800

    Add a param and correct the calculation logic.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 int main() {
     int a = 5;
     int b = 10;
-    int sum = a + b;
+    int c = 20; // 新增变量
+    int sum = a + b + c; // 修改计算逻辑
     std::cout << "Sum: " << sum << std::endl;
     return 0;
 }
